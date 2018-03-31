db0b6e6035ef1cc6f2344b6bba7ecb53f7b0cfc4
Nano67 <707935952@qq.com>
2018-03-31 22:54:20 +0800

    footer

--- Nekko0/polls/static/polls/comm/comm.js
+++ Nekko0/polls/static/polls/comm/comm.js
@@ -7,6 +7,11 @@ $(function(){
             //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
             $("#footer-outer").addClass("fixed-bottom");
         }
+
+        //设置文档正文宽度
+        var contentWidth = document.body.scrollWidth + "px";
+        document.getElementById("footer-outer").style.width = contentWidth;
+        document.getElementById("ft-style").style.width = contentWidth;
     }
     footerPosition();
     $(window).resize(footerPosition);

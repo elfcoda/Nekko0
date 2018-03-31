a94ae1826d5954fe0cf94a578278a1d7988bf0e8
Nano67 <707935952@qq.com>
2018-04-01 00:00:42 +0800

    footer

--- Nekko0/polls/static/polls/comm/comm.js
+++ Nekko0/polls/static/polls/comm/comm.js
@@ -4,6 +4,10 @@ $(function(){
         var contentWidth = document.body.scrollWidth + "px";
         document.getElementById("footer-outer").style.width = contentWidth;
         document.getElementById("ft-style").style.width = contentWidth;
+        //移动端判断
+	      var u = navigator.userAgent, app = navigator.appVersion;
+	      var mobile = !!u.match(/AppleWebKit.*Mobile.*/)||!!u.match(/AppleWebKit/)&&u.indexOf('QIHU')&&u.indexOf('Chrome')<0;
+	      var iPad = u.indexOf('iPad') > -1;
 
         $("#footer-outer").removeClass("fixed-bottom");
         var contentHeight = document.body.scrollHeight,//网页正文全文高度
@@ -11,6 +15,12 @@ $(function(){
         if(!(contentHeight > winHeight)){
             //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
             $("#footer-outer").addClass("fixed-bottom");
+            if(mobile&&!iPad)
+            {
+              document.getElementById("footer-outer").style.width = "100%";
+              document.getElementById("ft-style").style.width = "100%";
+              alert(1);
+            }
         }
 
     }

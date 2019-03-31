603974851484874f0d41ae88d063152fdfb45a2b
root <root@vultr.guest>
2019-03-31 08:07:58 +0000

    add start_do.sh

---
+++ start_do.sh
@@ -0,0 +1,7 @@
+cd /root/nekko/Nekko0
+git pull
+
+
+echo 'alias cdst="cd /root/nekko/Nekko0/Nekko0/polls/static/polls && ls"' >> ~/.bashrc
+echo 'alias cdte="cd /root/nekko/Nekko0/Nekko0/polls/templates/polls && ls"' >> ~/.bashrc
+echo 'alias cdpo="cd /root/nekko/Nekko0/Nekko0/polls && ls"' >> ~/.bashrc
